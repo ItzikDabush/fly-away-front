fbe4e9d8970f515b983c1e430f93471a5f7b2bba
Itzik Dabush <itikdb@gmail.com>
2019-11-19 08:50:29 +0200

    refactor to functinial component

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -2,20 +2,15 @@ import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
 
 const styles = theme => ({
   root: {
     background: theme.palette.primary.main,
     border: 0,
-    // borderRadius: 3,
-    // boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
     color: theme.palette.text.secondary,
     padding: "5px 30px",
     textAlign: "center",
-
-    // position: "absolute",
-    // bottom: "0px",
     width: "100%",
     fontSize: "0.8rem"
   },
@@ -26,9 +21,7 @@ const styles = theme => ({
   }
 });
 
-function Footer(props) {
-  const { classes } = props;
-
+const Footer = ({ classes }) => {
   return (
     <Paper elevation={5} square>
       <footer className={classes.root}>
@@ -53,6 +46,6 @@ function Footer(props) {
       </footer>
     </Paper>
   );
-}
+};
 
 export default withStyles(styles, { withTheme: true })(Footer);
