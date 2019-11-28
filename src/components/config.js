41298bcc1bb6b4fec05f70ee22ab5b66fdb537e3
Itzik Dabush <itikdb@gmail.com>
2019-11-28 22:28:57 +0200

    update config add local url

--- src/components/config.js
+++ src/components/config.js
@@ -1,4 +1,4 @@
-// const serverUrl = "https://git.heroku.com/fly-a-way.git";
-const serverUrl = "http://localhost:3001";
+const serverUrl = "https://git.heroku.com/fly-a-way.git";
+// const serverUrl = "http://localhost:3001";
 
 export { serverUrl };
